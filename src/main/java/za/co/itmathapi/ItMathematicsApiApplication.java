1d71c8024426c6c977713c27d25926631b7170ab
Omega-Andries <andrieschimule@gmail.com>
2021-10-22 10:37:00 +0200

    added neccessary files for deployment

--- src/main/java/za/co/itmathapi/ItMathematicsApiApplication.java
+++ src/main/java/za/co/itmathapi/ItMathematicsApiApplication.java
@@ -2,9 +2,10 @@ package za.co.itmathapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ItMathematicsApiApplication {
+public class ItMathematicsApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItMathematicsApiApplication.class, args);
